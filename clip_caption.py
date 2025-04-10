297159261942b740ec69fe49df51282c1c655523
Jing Li <lj20001130@gmail.com>
2025-04-10 14:40:35 +0100

    测试test.py中是否可以加载cuda

--- clip_caption.py
+++ clip_caption.py
@@ -6,7 +6,6 @@ from tqdm import tqdm
 
 # 设置设备
 device = "cuda" if torch.cuda.is_available() else "cpu"
-# device = "cuda" if torch.cuda.is_available() else "cpu"
 # 加载模型
 model, preprocess = clip.load("ViT-B/32", device=device)
 # model, preprocess = clip.load("ViT-B/32", device=device)
--- test.py
+++ test.py
@@ -1,2 +1,6 @@
 import clip
+import torch
 print(clip.available_models())
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print (device)
